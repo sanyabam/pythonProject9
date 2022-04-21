92653e6e3478d705cf8d1552d06f438ab6a450d5
sasha <sanyabam1983@mail.ru>
2022-04-21 12:37:05 +0300

    новый файл

---
+++ зад3.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and sea
+
+C_1 = (35, 78, 21, 37, 2, 98, 6, 100, 231)
+C_2 = (45, 21, 124, 76, 5, 23, 91, 234)
+if sum(C_1) > sum(C_2):
+    print('Сумма больше в кортеже 1')
+else:
+    print('Сумма больше в кортеже 2')
+print("Миним знач С_1=", min(C_1))
+print("Макс знач С_1=", max(C_1))
+print("Миним знач С_2=", min(C_2))
+print("Макс знач С_2=", max(C_2))
+print("Кобяков-енот")
+
